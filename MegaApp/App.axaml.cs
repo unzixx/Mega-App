44d4c7f6d6e6687c6e34bfe1ef48f5a09e04dd30
Benji <bathobenjaminlaszlo@gmail.com>
2026-01-18 13:44:08 +0100

    Upload

---
+++ MegaApp/App.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+namespace MegaApp;
+public partial class App : Application {
+    public override void Initialize() => AvaloniaXamlLoader.Load(this);
+    public override void OnFrameworkInitializationCompleted() {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+            desktop.MainWindow = new LoginWindow();
+        base.OnFrameworkInitializationCompleted();
+    }
+}
